4d5ea59f648cb085faf8f7e0bf669a51ff764ddc
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2017-07-26 16:06:23 -0400

    Enemies and scoring added

--- Captain Conundrum/GameScene.swift
+++ Captain Conundrum/GameScene.swift
@@ -14,9 +14,15 @@ enum GameState {
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    // Main variables and buttons
+    // Player and enemies
     var player: SKSpriteNode!
+    var initialMeteor: SKSpriteNode!
     var meteor: SKSpriteNode!
+    var satellite: SKSpriteNode!
+    var rocket: SKSpriteNode!
+    var ufo: SKSpriteNode!
+    
+    // Buttons
     var buttonPause: MSButtonNode!
     var boxPause: SKNode!
     var buttonContinue: MSButtonNode!
@@ -28,26 +34,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var scrollSpeed: CGFloat = 100
     
     // Other
+    var scoreLabel: SKLabelNode!
     var motionManager: CMMotionManager!
-    var meteorsHit = 0 // Keeps track of initial meteor herd
+    var initialMeteorsHit = 0 // Keeps track of initial meteor herd
     var messageTime: CFTimeInterval = 0 // In seconds
+    var spawnTimer: CFTimeInterval = 0
     var isTouching = false
     var touchTime: CFTimeInterval = 0
     var gameState: GameState = .active
     
+    var score = 0 {
+        didSet {
+            scoreLabel.text = String(score)
+        }
+    }
+    
     var attack: SKSpriteNode = {
-        let blast = SKSpriteNode()
+        let blast = SKSpriteNode(imageNamed: "laserRed02")
         blast.name = "attack"
-        blast.size.width = 15
-        blast.size.height = 40
-        blast.color = .orange
         blast.zPosition = 1
         blast.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: blast.size.width, height: blast.size.height))
         blast.physicsBody?.allowsRotation = false
         blast.physicsBody?.affectedByGravity = false
         blast.physicsBody?.categoryBitMask = 4
         blast.physicsBody?.collisionBitMask = 4
-        blast.physicsBody?.contactTestBitMask = 8
+        blast.physicsBody?.contactTestBitMask = 120 // In contact with all enemies
         return blast
     } ()
     
@@ -63,12 +74,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
         // Called immediately after scene is loaded into view
         player = childNode(withName: "player") as! SKSpriteNode
+        initialMeteor = childNode(withName: "initialMeteor") as! SKSpriteNode
         meteor = childNode(withName: "meteor") as! SKSpriteNode
+        satellite = childNode(withName: "satellite") as! SKSpriteNode
+        rocket = childNode(withName: "rocket") as! SKSpriteNode
+        ufo = childNode(withName: "ufo") as! SKSpriteNode
+        
         buttonPause = childNode(withName: "buttonPause") as! MSButtonNode
         boxPause = childNode(withName: "boxPause")
         buttonContinue = boxPause.childNode(withName: "buttonContinue") as! MSButtonNode
         buttonQuit = boxPause.childNode(withName: "buttonQuit") as! MSButtonNode
+        
         scrollLayer = childNode(withName: "scrollLayer")
+        scoreLabel = childNode(withName: "scoreLabel") as! SKLabelNode
         
         if gameState == .active {
             self.boxPause.isHidden = true
@@ -114,22 +132,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         motionManager.startAccelerometerUpdates()
     }
     
-    /*func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }*/
-    
     func shoot() {
         // Copies allow for multiple attacks on screen
         let multiAttack = attack.copy() as! SKSpriteNode
@@ -154,11 +156,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         isTouching = false
     }
     
-    /*override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }*/
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Called when an alert interrupts gameplay
+        if gameState != .active { return }
+        
+        isTouching = false
+    }
     
     func scrollWorld() {
+        // Endlessly create and destroy stars to give illusion of scrolling
         scrollLayer.position.y -= scrollSpeed * CGFloat(fixedDelta) // Moves scrollLayer along with child stars
         
         for star in scrollLayer.children as! [SKSpriteNode] {
@@ -172,6 +178,39 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    func spawnEnemy() {
+        // Randomly spawns an enemy falling from the top every second
+        if spawnTimer < 1 { return }
+        
+        let enemy = arc4random_uniform(4) // 4 enemies to choose from
+        let enemyPosition = CGPoint(x: CGFloat.random(min: -117, max: 117), y: 305)
+        
+        switch enemy {
+            case 0:
+                let newMeteor = meteor.copy() as! SKSpriteNode
+                newMeteor.position = enemyPosition
+                newMeteor.physicsBody?.velocity = CGVector(dx: 0, dy: -100)
+                addChild(newMeteor)
+            case 1:
+                let newSatellite = satellite.copy() as! SKSpriteNode
+                newSatellite.position = enemyPosition
+                newSatellite.physicsBody?.velocity = CGVector(dx: 0, dy: -200)
+                addChild(newSatellite)
+            case 2:
+                let newRocket = rocket.copy() as! SKSpriteNode
+                newRocket.position = enemyPosition
+                newRocket.physicsBody?.velocity = CGVector(dx: 0, dy: -300)
+                addChild(newRocket)
+            default:
+                let newUFO = ufo.copy() as! SKSpriteNode
+                newUFO.position = enemyPosition
+                newUFO.physicsBody?.velocity = CGVector(dx: 0, dy: -150)
+                addChild(newUFO)
+        }
+        
+        spawnTimer = fixedDelta
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         if gameState != .active { return }
@@ -179,22 +218,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         guard let motion = motionManager.accelerometerData else {
             return // Accelerometer isn't ready until the next frame
         }
-        player.position.x += CGFloat(Double((motion.acceleration.x)) * 15)
+        player.position.x += CGFloat(Double(motion.acceleration.x) * 15)
         
         scrollWorld()
+        spawnEnemy()
         
-        if attack.position.y >= 325 {
+        if attack.position.y >= 305 {
             attack.removeFromParent() // Remove attack when offscreen
         }
         
-        if meteorsHit >= 3 && messageTime > 0 { // Can hit more meteors without affecting start timer
+        if messageTime > 0 { // Can hit more meteors without affecting start timer
             messageTime += fixedDelta
         }
         
         // After 1 second, Start disappears
-        if messageTime >= 1.0 {
+        if messageTime >= 1 {
             startMessage.removeFromParent()
             messageTime = 0 // Reset time for any future messages
+        }
+        
+        // Waiting time before a new enemy shows up
+        if spawnTimer > 0 {
+            spawnTimer += fixedDelta
         }
         
         if isTouching {
@@ -213,17 +258,55 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         let contactA = contact.bodyA
         let contactB = contact.bodyB
-        let nodeA = contactA.node!
-        let nodeB = contactB.node!
+        // If player decides to go insane, nodes won't even be ready!
+        guard let nodeA = contactA.node else {
+            return
+        }
+        guard let nodeB = contactB.node else {
+            return
+        }
+        
+        // Player is doing damage
+        if nodeA.name == "attack" && nodeB.name == "initialMeteor" || nodeA.name == "initialMeteor" && nodeB.name == "attack" {
+            nodeA.removeFromParent()
+            nodeB.removeFromParent()
+            initialMeteorsHit += 1
+            if initialMeteorsHit == 3 {
+                addChild(startMessage) // Player has completed tutorial section
+                scoreLabel.isHidden = false
+                messageTime += fixedDelta
+                spawnTimer += fixedDelta
+            }
+        }
         
         if nodeA.name == "attack" && nodeB.name == "meteor" || nodeA.name == "meteor" && nodeB.name == "attack" {
             nodeA.removeFromParent()
             nodeB.removeFromParent()
-            meteorsHit += 1
-            if meteorsHit == 3 {
-                addChild(startMessage) // Player has completed tutorial section
-                messageTime += fixedDelta
-            }
+            score += 1
+        }
+        
+        if nodeA.name == "attack" && nodeB.name == "satellite" || nodeA.name == "satellite" && nodeB.name == "attack" {
+            nodeA.removeFromParent()
+            nodeB.removeFromParent()
+            score += 5
+        }
+        
+        if nodeA.name == "attack" && nodeB.name == "rocket" || nodeA.name == "rocket" && nodeB.name == "attack" {
+            nodeA.removeFromParent()
+            nodeB.removeFromParent()
+            score += 10
+        }
+        
+        if nodeA.name == "attack" && nodeB.name == "ufo" || nodeA.name == "ufo" && nodeB.name == "attack" {
+            nodeA.removeFromParent()
+            nodeB.removeFromParent()
+            score += 20
+        }
+        
+        // Player is taking damage (except with the boundaries)
+        if nodeA.name == "player" && nodeB.name != "boundary" || nodeA.name != "boundary" && nodeB.name == "player" {
+            if nodeA.name != "player" && nodeA.name != "boundary" { nodeA.removeFromParent() }
+            else if nodeB.name != "player" && nodeB.name != "boundary" { nodeB.removeFromParent() }
         }
     }
 }
